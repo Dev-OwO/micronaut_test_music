a073663acc2d727736b25be2c123fda57e078128
Vlad <11traize@gmail.com>
2026-02-11 21:55:26 +0800

    Создан проект с micronaut CLI

---
+++ src/test/java/micronaut_test_music/Micronaut_test_musicTest.java
@@ -0,0 +1,21 @@
+package micronaut_test_music;
+
+import io.micronaut.runtime.EmbeddedApplication;
+import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
+
+import jakarta.inject.Inject;
+
+@MicronautTest
+class Micronaut_test_musicTest {
+
+    @Inject
+    EmbeddedApplication<?> application;
+
+    @Test
+    void testItWorks() {
+        Assertions.assertTrue(application.isRunning());
+    }
+
+}
