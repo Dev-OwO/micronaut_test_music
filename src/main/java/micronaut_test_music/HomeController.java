3f13091f6387a3a90c11683842493ab7255a2a07
Vlad <11traize@gmail.com>
2026-02-26 22:34:45 +0800

    Добавлен Thymeleaf, страница /

---
+++ src/main/java/micronaut_test_music/HomeController.java
@@ -0,0 +1,15 @@
+package micronaut_test_music;
+
+import io.micronaut.http.annotation.Controller;
+import io.micronaut.http.annotation.Get;
+import io.micronaut.views.View;
+
+@Controller
+public class HomeController {
+
+    @Get("/")
+    @View("index")
+    public void index() {
+        
+    }
+}
